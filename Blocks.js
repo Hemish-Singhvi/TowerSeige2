4c7ba0eb95d8a01a84c1211573881b9a5029b0b0
Hemish-Singhvi <73469894+Hemish-Singhvi@users.noreply.github.com>
2020-12-14 14:15:39 +0530

    Add files via upload

---
+++ Blocks.js
@@ -0,0 +1,36 @@
+class block {
+    constructor(x, y){
+        var options = {
+            'restitution':0.8,
+			'density':0.01
+        }
+        this.x = x;
+        this.y = y;
+        this.width = 30;
+        this.height = 40;
+        this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
+        this.Visiblity = 255;
+		World.add(world, this.body);
+		colors = ["green", "red", "blue", "purple", "orange"];
+		color = random(colors);
+    }
+    display(){        
+        rectMode(CENTER);
+        if(this.body.speed > 2.5){
+            push();
+            World.remove(world,this.body);
+            this.Visibility=this.Visibility-5;
+            tint(255,this.Visibility);
+            var pos=this.body.position;
+            
+            pop();  
+            
+        }
+        else{
+            var pos =this.body.position;
+            fill(color);
+            rect(pos.x, pos.y, this.width, this.height);
+        }
+    console.log(this.Visibility)
+    }
+}
